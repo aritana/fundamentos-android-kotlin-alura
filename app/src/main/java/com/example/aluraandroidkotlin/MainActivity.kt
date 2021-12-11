9186426882238adb56fee336d1dcd55826d7a3ab
Aritana Noara <arinoara@yahoo.com.br>
2021-12-11 01:07:38 -0300

    using OnCreate, one of the life cycles of Activity to show us a welcome message

--- app/src/main/java/com/example/aluraandroidkotlin/MainActivity.kt
+++ app/src/main/java/com/example/aluraandroidkotlin/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.example.aluraandroidkotlin
 
 import android.app.Activity
+import android.os.Bundle
+import android.widget.Toast
 
 //configurar tambem no manifest
-class MainActivity: Activity() {
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Toast.makeText(this, "Bem vindo(a)!!", Toast.LENGTH_SHORT).show()
+    }
 }
