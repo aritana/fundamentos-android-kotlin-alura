22cc5ab2e0e8138e7285b368f909a08659901194
Aritana Noara <arinoara@yahoo.com.br>
2021-12-11 22:37:58 -0300

    searching for the views of the layoult

--- app/src/main/java/com/example/aluraandroidkotlin/MainActivity.kt
+++ app/src/main/java/com/example/aluraandroidkotlin/MainActivity.kt
@@ -14,5 +14,18 @@ class MainActivity : Activity() {
         Toast.makeText(this, "Bem vindo(a)!!", Toast.LENGTH_SHORT).show()
 
         setContentView(R.layout.activity_main)
+
+        //cada text view é uma view
+        //para acessar uma vie, utilizamos o comando abaixo
+
+        val nome = findViewById<TextView>(R.id.nome)
+        nome.text = "Cesta de Frutas"
+
+        val descricao = findViewById<TextView>(R.id.descricao)
+        descricao.text = "Laranja, manga e uva"
+
+        val valor = findViewById<TextView>(R.id.valor)
+        valor.text = "19.90"
+
     }
 }
